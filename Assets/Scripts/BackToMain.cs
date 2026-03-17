6d6d9125884287ef7f0abb9170260d11aa742a53
RowardDorrestijn <94120545+RowardDorrestijn@users.noreply.github.com>
2026-03-17 16:19:21 +0100

    Update (#28)

--- Assets/Scripts/BackToMain.cs
+++ Assets/Scripts/BackToMain.cs
@@ -6,6 +6,6 @@ public class BackToMain : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void NextScene(string scene)
     {
-        SceneManager.LoadScene(scene);
+        SceneManager.LoadScene(0);
     }
 }
